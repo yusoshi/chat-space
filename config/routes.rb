ead636a57350ade71ddc3452783c4f61869953ca
yusoshi <nsymgnk0405@yahoo.co.jp>
2017-02-18 13:50:53 +0900

    Modefy rooting

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'chat_groups/messages' => 'chat_groups#messages'
-  resources :groups
+  resources :groups, only: [:new, :edit]
 end
