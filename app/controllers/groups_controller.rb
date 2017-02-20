f3f40e227cef075d5a757d855c9aaead9de36297
yusoshi <nsymgnk0405@yahoo.co.jp>
2017-02-20 16:31:07 +0900

    Create group function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -14,6 +14,6 @@ class GroupsController < ApplicationController
   private
 
   def create_params
-    params.require(:group).permit(:name)
+    params.require(:group).permit(:name, {:user_ids => []})
   end
 end
