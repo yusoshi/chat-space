ab4efcdeb24590d282fa6823708c2eb196b34374
yusoshi <nsymgnk0405@yahoo.co.jp>
2017-02-20 18:15:33 +0900

    Modify rooting

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,4 @@
 class Group < ApplicationRecord
   has_many :groups_users
   has_many :users, through: :groups_users
-  accepts_nested_attributes_for :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   has_many :groups_users
   has_many :groups, through: :groups_users
-  accepts_nested_attributes_for :groups
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
