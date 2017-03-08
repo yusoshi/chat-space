bcef8aec7d6a311b5a4be2ed4068ed3e3840913b
yusoshi <nsymgnk0405@yahoo.co.jp>
2017-03-08 12:42:14 +0900

    Create messages table

---
+++ db/migrate/20170307113040_create_messages.rb
@@ -0,0 +1,10 @@
+class CreateMessages < ActiveRecord::Migration[5.0]
+  def change
+    create_table :messages do |t|
+      t.text           :body, presence: true
+      t.references     :group, foreign_key: true
+      t.references     :user, foreign_key: true
+      t.timestamps
+    end
+  end
+end
