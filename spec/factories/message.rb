d3d6eeea0ea3199caf267a961f94a10408ab2be4
yusoshi <nsymgnk0405@yahoo.co.jp>
2017-03-12 17:56:27 +0900

    Set random number in user_id and group_id between 1 to 1000

--- spec/factories/message.rb
+++ spec/factories/message.rb
@@ -2,8 +2,8 @@ FactoryGirl.define do
 
   factory :message do
     body              "Hello"
-    group_id          1
-    user_id           1
+    group_id          { Faker::Number.between(1, 1000) }
+    user_id           { Faker::Number.between(1, 1000) }
   end
 
 end
