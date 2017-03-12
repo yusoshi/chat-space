9cf0dc14edff939329b8b2ccfd08ea577d524512
yusoshi <nsymgnk0405@yahoo.co.jp>
2017-03-12 17:34:26 +0900

    Write the code testing it is valid with a body

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -7,5 +7,10 @@ describe Message do
       message.valid?
       expect(message.errors[:body]).to include('を入力してください')
     end
+
+    it "is valid with a body" do
+      message = build(:message)
+      expect(message).to be_valid
+    end
   end
 end
