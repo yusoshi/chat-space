958c5e5306a4d8751c591fd0178b506e7582a490
yusoshi <nsymgnk0405@yahoo.co.jp>
2017-03-12 17:55:36 +0900

    Modify argument in body test

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe Message do
   describe '#create' do
     it "is invalid without a body" do
-      message = build(:message, body: nil)
+      message = build(:message, body: '')
       message.valid?
       expect(message.errors[:body]).to include('を入力してください')
     end
