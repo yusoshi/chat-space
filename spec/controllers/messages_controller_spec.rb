b959b4c9aa2cfee38e78f881321bd4ee78f89615
yusoshi <nsymgnk0405@yahoo.co.jp>
2017-03-18 14:36:56 +0900

    Apply let to group

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe MessagesController, type: :controller do
 
   let(:user) { create(:user) }
+  let(:group) { create(:group) }
 
   describe 'GET #index' do
     before do
@@ -10,16 +11,15 @@ describe MessagesController, type: :controller do
     end
 
     it "populates an array of messages ordered by created_at DESC" do
-      group = create(:group)
       messages = create_list(:message, 5, user_id: user.id, group_id: group.id)
       get :index, params: { group_id: group.id }
       expect(assigns(:messages)).to match(messages)
     end
 
     it "renders the :index template" do
-      group = create(:group)
       get :index, params: { group_id: group.id }
       expect(response).to render_template :index
     end
   end
+
 end
