1c1def05aa7381f366f190ab2a251210193c28f2
yusoshi <nsymgnk0405@yahoo.co.jp>
2017-03-22 20:02:05 +0900

    Modify notation

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,9 +1,17 @@
 $(function() {
   function buildHTML(message) {
     var list = $('<li class="chat-main__messages-area__message-section__message-list">');
+    // ユーザーネームの取得とHTMLと挿入
     var userName = $('.chat-main__footer__form___current-user-name').val();
     var userNameHTML = '<p class="chat-main__messages-area__message-section__message-list__head__name">' + userName + '</p>';
-    var messageTimeHTML = '<p class="chat-main__messages-area__message-section__message-list__head__post-date">' + message.created_at + "</p>";
+    // 投稿日時の取得とHTML挿入
+    var messageTime1 = message.created_at.replace(/-/g, '/');
+    var messageTime2 = messageTime1.replace('T', ' ');
+    var messageTime3 = messageTime2.replace(/.\d\d\d.$/, '');
+
+    var messageTimeHTML = '<p class="chat-main__messages-area__message-section__message-list__head__post-date">' + messageTime3 + "</p>";
+
+    // メッセージ情報のHTML整形
     var message_info = list.append('<div class="chat-main__messages-area__message-section__message-list__head">' + userNameHTML + messageTimeHTML + '</div>');
     var body = list.append('<p class="chat-main__messages-area__message-list__message">' + message.body + '</p>');
 
