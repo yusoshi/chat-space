eefcfb9ca00d89fd12d3800781c8dcef57429d3e
yusoshi <nsymgnk0405@yahoo.co.jp>
2017-04-02 00:54:15 +0900

    Modify the group edit to show group members  in advance

--- app/assets/javascripts/groups_users.js
+++ app/assets/javascripts/groups_users.js
@@ -1,16 +1,4 @@
 $(function() {
-
-  // todo list
-  // @@ = done, @ = WIP
-
-  // @@追加ボタン押すと下に表示される
-  // @@Saveボタンを押すと非同期通信でグループ情報を送信
-  // @@削除できる
-  // @@同じ名前の人を何回も出さないようにする(げ・ん・きってうつと…)
-  // @@検索候補から外れたユーザーを検索候補から消す
-  // editのときにすでにグループに所属しているユーザーを表示する
-
-
   // ユーザーの検索結果と「追加」ボタンを表示
   function userList(lists) {
     $(lists).each(function(i, list) {
@@ -57,7 +45,7 @@ $(function() {
 
     var div1 = $('#chat-group-users');
     var div2 = $('<div class="chat-group-user clearfix">');
-    var addInput = div2.append('<input type="hidden", name="group[users_id][]", id="chat-group-user-' + userId + '", value=' + userId + '>');
+    var addInput = div2.append('<input type="hidden" name="group[users_id][]" id="chat-group-user-' + userId + '" value=' + userId + '>');
     var addName = div2.append('<p class="chat-group-user__name">' + userName);
     var addBtn = div2.append('<a class="chat-group-user__btn chat-group-user__btn--remove" user_id=' + userId + ' user_name=' + userName +'>削除</a>');
     var div1WithName = div1.append(addName);
@@ -70,20 +58,48 @@ $(function() {
     $(this).parent().remove();
    })
 
-  // Saveボタンを押すと非同期通信でグループ情報を送信
-  $('#new_group').on('submit', function(e) {
+  // Saveボタンを押すと非同期通信でグループ情報を送信(新規作成)
+  $('.new_group').on('submit', function(e) {
     e.preventDefault();
-    var currentUrl = location.href;
-
     var user_ids = [];
     $('.chat-group-user').each(function() {
-      user_ids.push($('chat-group-user__btn--remove').attr('user_id'))
+      user_ids.push($(this).find('input').val());
     });
     var name = $('#chat_group_name').val();
 
     $.ajax({
       type: 'POST',
       url: '/groups.json',
+      data: {
+        group: {
+          name: name,
+          user_ids: user_ids
+        }
+      }
+    })
+    .done(function(data) {
+      window.location.href = "/groups/" + data.id + "/messages";
+    })
+    .fail(function() {
+      alert('送信に失敗しました。')
+    })
+  });
+
+  // Saveボタンを押すと非同期通信でグループ情報を送信(編集時)
+  $('.edit_group').on('submit', function(e) {
+    e.preventDefault();
+    var user_ids = [];
+    $('.chat-group-user').each(function() {
+      user_ids.push($(this).find('input').val());
+    });
+    var name = $('#chat_group_name').val();
+
+    var currentUrl = location.href;
+    var groupId = currentUrl.match(/\/(\d\d)\//)[1];
+
+    $.ajax({
+      type: 'PATCH',
+      url: '/groups/' + groupId +'.json',
       data: {
         group: {
           name: name,
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -10,7 +10,7 @@ before_action :get_group, only: [:edit, :update]
     if @group.save
       respond_to do |format|
         format.html { redirect_to group_messages_path(@group), notice: "グループを作成しました。" }
-        format.json { render json: @group }
+        format.json { render json: @group, notice: "グループを作成しました。"  }
       end
     else
       flash.now[:alert] = "グループが保存できませんでした。"
@@ -19,11 +19,15 @@ before_action :get_group, only: [:edit, :update]
   end
 
   def edit
+    @users = @group.users
   end
 
   def update
     if @group.update(create_params)
-      redirect_to group_messages_path(@group), notice: "グループを更新しました。"
+      respond_to do |format|
+        format.html { redirect_to group_messages_path(@group), notice: "グループを更新しました。"}
+        format.json { render json: @group, notice: "グループを更新しました。"}
+      end
     else
       flash.now[:alert] = "グループ情報を更新できませんでした。"
       render action: :edit
