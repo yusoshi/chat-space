9399b3c187ba2d7876ad6df0121ead96cb43802c
yusoshi <nsymgnk0405@yahoo.co.jp>
2017-04-02 21:57:25 +0900

    Adapt template literal notation

--- app/assets/javascripts/groups_users.js
+++ app/assets/javascripts/groups_users.js
@@ -1,13 +1,13 @@
 $(function() {
+
   // ユーザーの検索結果と「追加」ボタンを表示
   function userList(lists) {
     $(lists).each(function(i, list) {
-      var div1 = $('.chat-group-form__field--right__user-search-result');
-      var div2 = $('<div class="chat-group-user chat-group-user-search clearfix">');
-      var addName = div2.append('<p class="chat-group-user__name">' + list.name);
-      var addBtn = div2.append('<a class="chat-group-user__btn chat-group-user__btn--add" user_id=' + list.id + ' user_name=' + list.name +'>追加</a>');
-      var div1WithName = div1.append(addName);
-      var div1WithNameAndBtn = div1.append(div1WithName);
+      var div = $(`.chat-group-form__field--right__user-search-result`);
+        div.append(
+          `<div class="chat-group-user chat-group-user-search clearfix">
+            <p class="chat-group-user__name">${list.name}</p>
+            <a class="chat-group-user__btn chat-group-user__btn--add" user_id=${list.id} user_name=${list.name} >追加</a>`);
     });
   }
 
@@ -43,13 +43,12 @@ $(function() {
     var userId = $(this).attr('user_id');
     var userName = $(this).attr('user_name');
 
-    var div1 = $('#chat-group-users');
-    var div2 = $('<div class="chat-group-user clearfix">');
-    var addInput = div2.append('<input type="hidden" name="group[users_id][]" id="chat-group-user-' + userId + '" value=' + userId + '>');
-    var addName = div2.append('<p class="chat-group-user__name">' + userName);
-    var addBtn = div2.append('<a class="chat-group-user__btn chat-group-user__btn--remove" user_id=' + userId + ' user_name=' + userName +'>削除</a>');
-    var div1WithName = div1.append(addName);
-    var div1WithNameAndBtn = div1.append(div1WithName);
+    var div = $('#chat-group-users');
+    div.append(`
+      <div class="chat-group-user clearfix">
+        <input type="hidden" name="group[users_id][]" id="chat-group-user-${userId}" value=${userId}
+        <p class="chat-group-user__name">${userName}
+        <a class="chat-group-user__btn chat-group-user__btn--remove" user_id=${userId} user_name=${userName}>削除</a>`);
     $(this).parent().remove();
   });
 
