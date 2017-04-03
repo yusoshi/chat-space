6828db5fab0d664d751fab316ac8d079763d13ad
yusoshi <nsymgnk0405@yahoo.co.jp>
2017-04-03 21:18:31 +0900

    Remove comment out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,5 @@ class UsersController < ApplicationController
   @results = User.where('name LIKE(?)', "%#{ params[:q] }%")
   render 'user/search', formats: [:json], handler: [:jbuilder]
 
-  # results.each do |result|
-  #   lists << { name: result.name, id: result.id}  if result.name && result.id
-  # end
-
-  # render json: { lists: lists }
-
-
   end
 end
