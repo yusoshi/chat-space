934ec1e0a07ba99472c51401983220e11c79e1d9
yusoshi <nsymgnk0405@yahoo.co.jp>
2017-04-13 21:29:44 +0900

    Add capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.8.0"
 
 set :application, "chat-space"
-set :repo_url, "git@github.com:chat-space.git"
+set :repo_url, "git@github.com:yusoshi/chat-space.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
@@ -10,7 +10,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['.ssh/genkey.pem']
+                  keys: ['~/.ssh/genkey.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
