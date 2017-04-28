94d87b1c6d7eafd825856dddccf4263c2ba053a2
yusoshi <nsymgnk0405@yahoo.co.jp>
2017-04-28 16:23:20 +0900

    change

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,8 +24,8 @@ namespace :deploy do
 set :default_env, {
   rbenv_root: "/usr/local/rbenv",
   path: "/anaconda/bin:/Users/nishiyamagenki/.rbenv/shims:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin",
-  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
-  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
 }
 set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
 set :sidekiq_queue, :carrierwave
